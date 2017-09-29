d9dbecb3652573f3140b954d7c8c24c97a27a985
Ade Yahya <adeyahyapprasetyo@idntimes.com>
2017-09-30 00:34:31 +0700

    add more data from pokemon-species

--- pages/post.js
+++ pages/post.js
@@ -6,19 +6,45 @@ import { nameNicer, getPokemonId } from '../lib'
 import { Link } from '../routes'
 
 class Post extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      profile: []
+    }
+  }
+
   static async getInitialProps({query, req}) {
     let theId = query.id.replace(/\s/g,'').toLowerCase()
     const res = await fetch(`http://pokeapi.salestock.net/api/v2/pokemon/${theId}`)
     const data = await res.json()
+
     return {
       data: data,
       status: res.status,
+      theId: theId,
       idType: isNaN(theId) ? "string" : "number"
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     console.log(this.props)
+    const res = await fetch(`http://pokeapi.salestock.net/api/v2/pokemon-species/${this.props.theId}`)
+    const data = await res.json()
+    if (res.status === 200) {
+      this.setState({
+        profile: [
+          {name: 'Base Happiness', value: data.base_happiness},
+          {name: 'Capture Rate', value: data.capture_rate + " %"},
+          {name: 'color', value: data.color.name},
+          {name: 'Hatch Counter', value: data.hatch_counter},
+          {name: 'Generation', value: data.generation.name},
+          {name: 'Habitat', value: data.habitat.name},
+          {name: 'Growth Rate', value: data.growth_rate.name},
+          {name: 'Egg Groups', value: data.egg_groups.map(x => x.name).join(', ')},
+        ]
+      })
+    }
+    console.log(this.state.profile)
   }
 
   render() {
@@ -55,16 +81,47 @@ class Post extends React.Component {
         {_.filter(sprites, o => o).filter(x => x != sprites.front_default).map((image, index) =>
           <img key={index} src={image}/>
         )}
-        <h2>Abilities</h2>
-        <ul>
-          {abilities.map((x,i) =>
-            <li key={i}>
-              <Link route={`/ability/${x.ability.name}`}>
-                <a>{nameNicer(x.ability.name)}</a>
-              </Link>
-            </li>
+
+        <br/><br/>
+        <table>
+          <tbody>
+            <tr>
+              <td>Base Experience</td>
+              <td>{this.props.data.base_experience}</td>
+            </tr>
+            <tr>
+              <td>Height</td>
+              <td>{(this.props.data.height).toString().split('').join('.')} m</td>
+            </tr>
+            <tr>
+              <td>Weight</td>
+              <td>{this.props.data.weight} Kg</td>
+            </tr>
+          </tbody>
+          <tbody>
+          {this.state.profile.map((item,i) =>
+            <tr key={i}>
+              <td>{item.name}</td>
+              <td>{item.value}</td>
+            </tr>
           )}
-        </ul>
+          </tbody>
+        </table>
+
+        <h2>Abilities</h2>
+        <table>
+          <tbody>
+            {abilities.map((x,i) =>
+              <tr key={i}>
+                <td>
+                  <Link route={`/ability/${x.ability.name}`}>
+                    <a>{nameNicer(x.ability.name)}</a>
+                  </Link>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
 
         <h2>Stats</h2>
         <table>
@@ -85,11 +142,21 @@ class Post extends React.Component {
         </table>
 
         <h2>Moves</h2>
-        {moves.map((x,i) =>
-          <span key={i}>{nameNicer(x.move.name)}, </span>
-        )}
+        <p className="moves">
+          { moves.map(x => nameNicer(x.move.name)).join(', ') }
+        </p>
 
         <style jsx>{`
+          p.moves {
+            line-height: 2;
+            text-transform: capitalize;
+          }
+          a {
+            color: #3f51b5;
+            font-weight: bold;
+            font-size: 1.1rem;
+            text-transform: capitalize;
+          }
           table {
             width: 100%;
             max-width: 100%;
