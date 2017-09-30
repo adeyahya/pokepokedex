23ba89c693cf4dadbfc810c5ce2d22abd29d7512
Ade Yahya <adeyahyapprasetyo@idntimes.com>
2017-09-30 20:16:11 +0700

    remove pm2, mirror API

--- server.js
+++ server.js
@@ -6,10 +6,49 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const routes = require('./routes')
 const handle = routes.getRequestHandler(app)
+const referrerPolicy = require('referrer-policy')
+const fetch = require('isomorphic-unfetch')
 
 app.prepare()
 .then(() => {
   const server = express()
+  server.use(referrerPolicy({ policy: 'same-origin' }))
+  server.get('/api/pokemon', async (req, res) => {
+    const offset = req.query.offset ? '?offset=' + req.query.offset : ''
+    const resp = await fetch(`http://pokeapi.salestock.net/api/v2/pokemon/${offset}`)
+    const data = await resp.json()
+    if (resp.status === 200)
+      return res.json(data)
+
+    return res.status(resp.status).json({message: 'error'})
+  })
+
+  server.get('/api/pokemon/:id', async (req, res) => {
+    const resp = await fetch(`http://pokeapi.salestock.net/api/v2/pokemon/${req.params.id}`)
+    const data = await resp.json()
+    if (resp.status === 200)
+      return res.json(data)
+
+    return res.status(resp.status).json({message: 'error'})
+  })
+
+  server.get('/api/ability/:name', async (req, res) => {
+    const resp = await fetch(`http://pokeapi.salestock.net/api/v2/ability/${req.params.name}`)
+    const data = await resp.json()
+    if (resp.status === 200)
+      return res.json(data)
+
+    return res.status(resp.status).json({message: 'error'})
+  })
+
+  server.get('/api/species/:name', async (req, res) => {
+    const resp = await fetch(`http://pokeapi.salestock.net/api/v2/pokemon-species/${req.params.name}`)
+    const data = await resp.json()
+    if (resp.status === 200)
+      return res.json(data)
+
+    return res.status(resp.status).json({message: 'error'})
+  })
 
   server.get('*', (req, res) => {
     return handle(req, res)
